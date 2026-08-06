e9f8eb54826403187dea65141ecde5d4777d7053
Jay_Panchal <65846507+jay-panchal2711@users.noreply.github.com>
2026-08-06 20:21:16 +0530

    Time: 1057 ms (5.81%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,26 +1,26 @@
 # Write your MySQL query statement below
 
--- With CTE as (
---     SELECT user_id,
---     Sum(CASE When action = "confirmed" THEN 1 ELSE 0 END) as cnt
---     FROM Confirmations
---     Group by user_id
--- )
+With CTE as (
+    SELECT user_id,
+    Sum(CASE When action = "confirmed" THEN 1 ELSE 0 END) as cnt
+    FROM Confirmations
+    Group by user_id
+)
 
--- SELECT s.user_id, Round(IFNULL(ct.cnt/Count(c.action), 0),2) as confirmation_rate
--- FROM Signups s
--- LEFT Join Confirmations c
--- ON s.user_id = c.user_id
--- LEFT Join CTE ct
--- ON s.user_id = ct.user_id
--- Group By s.userId;
-
-
-
-
-SELECT s.user_id,
-ROUND(IFNULL(SUM(CASE WHEN c.action = "confirmed" Then 1 ELSE 0 END) /count(c.action), 0),2) as confirmation_rate
+SELECT s.user_id, Round(IFNULL(ct.cnt/Count(c.action), 0),2) as confirmation_rate
 FROM Signups s
-LEFT Join confirmations c
-On s.user_id = c.user_id
-Group by s.user_id;
+LEFT Join Confirmations c
+ON s.user_id = c.user_id
+LEFT Join CTE ct
+ON s.user_id = ct.user_id
+Group By s.user_id;
+
+
+
+
+-- SELECT s.user_id,
+-- ROUND(IFNULL(SUM(CASE WHEN c.action = "confirmed" Then 1 ELSE 0 END) /count(c.action), 0),2) as confirmation_rate
+-- FROM Signups s
+-- LEFT Join confirmations c
+-- On s.user_id = c.user_id
+-- Group by s.user_id;
