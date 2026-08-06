bcb85ef8151131845b99d0c049af266434f19991
Jay_Panchal <65846507+jay-panchal2711@users.noreply.github.com>
2026-08-06 22:40:00 +0530

    Time: 262 ms (79.41%), Space: 100.3 MB (80.15%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.py
@@ -0,0 +1,33 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+
+  def remainingMethods(
+      self, n: int, k: int, invocations: List[List[int]]
+  ) -> List[int]:
+
+    adj = [[] for _ in range(n)]
+    for u, v in invocations:
+      adj[u].append(v)
+
+
+    suspicious = set()
+    queue = deque([k])
+    suspicious.add(k)
+
+    while queue:
+      curr = queue.popleft()
+      for neighbor in adj[curr]:
+        if neighbor not in suspicious:
+          suspicious.add(neighbor)
+          queue.append(neighbor)
+
+
+    for u, v in invocations:
+      if u not in suspicious and v in suspicious:
+        return list(range(n))
+
+
+    return [i for i in range(n) if i not in suspicious]
